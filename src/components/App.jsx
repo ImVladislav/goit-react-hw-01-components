04aa5bcee94b5c378288a044d6d67b5b65bf2fe0
ImVladislav <nafanya102@gmail.com>
2022-11-21 16:30:55 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,6 @@ import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
-import { Tag } from './Profile';
 
 
 export const App = () => {
